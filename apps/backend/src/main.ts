ee36e5107c15672e7128ee09448d4800a274411d
Murad <muradlu@tutamail.com>
2026-04-16 15:02:00 +0400

    fix: remove deployment from workflows

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,9 +8,9 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
