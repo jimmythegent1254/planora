697c4db5bbb5ddbe540d4c93346b67d5e2027486
Murad <muradlu@tutamail.com>
2026-04-21 10:09:37 +0400

    add dropdown menu to auth

---
+++ apps/frontend/components/navbar/navbar-dropdown.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { authClient } from "@/lib/auth-client";
+import { Session } from "better-auth";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
+import { Avatar, AvatarBadge, AvatarFallback, AvatarImage } from "../ui/avatar";
+
+type NavbarDropdownType = {
+  session: Session;
+};
+
+export function NavbarDropdown({ session }: NavbarDropdownType) {
+  const router = useRouter();
+  const [isSigningOut, setIsSigningOut] = useState(false);
+
+  const handleSignOut = async () => {
+    setIsSigningOut(true);
+    try {
+      await authClient.signOut({
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Signed out successfully");
+            router.refresh();
+          },
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to sign out");
+    } finally {
+      setIsSigningOut(false);
+    }
+  };
+  console.log(session);
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="cursor-pointer">
+          <AvatarImage
+            src="https://github.com/evilrabbit.png"
+            alt="@evilrabbit"
+          />
+          <AvatarFallback>ER</AvatarFallback>
+          <AvatarBadge className="bg-green-600 dark:bg-green-800" />
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40" align="start">
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuItem>
+            Profile
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Billing
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem>Email</DropdownMenuItem>
+                <DropdownMenuItem>Message</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>More...</DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+          <DropdownMenuItem>
+            New Team
+            <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>GitHub</DropdownMenuItem>
+          <DropdownMenuItem>Support</DropdownMenuItem>
+          <DropdownMenuItem disabled>API</DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem
+            disabled={isSigningOut}
+            onClick={handleSignOut}
+            className="text-slate-900"
+          >
+            {isSigningOut ? (
+              <span className="opacity-50">Loading...</span>
+            ) : (
+              "Sign Out"
+            )}
+            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
---
+++ apps/frontend/components/navbar/navbar.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { Bell, Command, Plus, Search } from "lucide-react";
+import { useRouter } from "next/navigation";
+import Logo from "../logo";
+import { Button } from "../ui/button";
+import { NavbarDropdown } from "./navbar-dropdown";
+
+const Navbar = () => {
+  const router = useRouter();
+  const { data: session } = authClient.useSession();
+  const navItems = ["Browse Events", "Dashboard", "Analytics"];
+  const navButtonClassName =
+    "bg-transparent text-slate-500 hover:text-slate-600 font-semibold cursor-pointer";
+
+  return (
+    <div className="bg-white py-4 px-96 flex justify-between items-center border border-b-blue-200">
+      <div className="w-3/12">
+        <Logo />
+      </div>
+      <div className="flex items-center justify-center w-5/12 mr-16">
+        {navItems.map((item) => (
+          <Button key={item} className={navButtonClassName}>
+            {item}
+          </Button>
+        ))}
+      </div>
+
+      <div className="w-4/12 flex gap-5 items-center justify-end">
+        <Button className=" text-slate-600 border cursor-pointer bg-slate-50 hover:bg-slate-100 hover:text-slate-900 border-slate-300 w-4/12">
+          <Search />
+          <span className="font-light text-[13px]">Search...</span>
+          <div className="flex border border-slate-400 text-slate-500 bg-white rounded-sm text-xs items-center px-1 py-0.5">
+            <Command className="w-3! h-3!" />
+            <span className="text-[10px]">K</span>
+          </div>
+        </Button>
+
+        <Button className="bg-transparent text-rose-600 hover:bg-slate-100 cursor-pointer p-2 py-4">
+          <Bell className="w-5! h-5!" />
+        </Button>
+
+        <Button className="bg-rose-600 hover:bg-rose-500 cursor-pointer">
+          <Plus />
+          <span className="text-[13px] font-semibold">Create Event</span>
+        </Button>
+
+        {!session ? (
+          <Button
+            onClick={() => router.push("/login")}
+            className="bg-transparent hover:bg-slate-100 text-slate-900 font-semibold cursor-pointer"
+          >
+            Sign In
+          </Button>
+        ) : (
+          <NavbarDropdown session={session.session} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
